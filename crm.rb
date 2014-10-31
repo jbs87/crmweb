9ccdc6400d251d3a5294ebea120ac05363589541
Joshua Sebastian <jbs87@hotmail.com>
2014-10-31 16:37:35 -0400

    Final web CRM

--- crm.rb
+++ crm.rb
@@ -1,16 +1,36 @@
 require 'sinatra'
-require_relative 'contact'
-require_relative 'rolodex'
+require 'data_mapper'
 
-@@rolodex = Rolodex.new
+DataMapper.setup(:default, "sqlite3:database.sqlite3")
+class Contact
+
+	include DataMapper::Resource
+
+	property :id, Serial
+	property :first_name, String
+	property :last_name, String
+	property :email, String
+	property :note, String
+
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
 
 get '/' do
 	@crm_app_name = "the best CRM"
 	erb :index
 end
 
+
 get '/contacts' do
-	erb :contacts
+	@contacts = Contact.all
+	erb :contacts1
+end
+
+get '/contacts/modify' do
+	@contacts = Contact.all
+	erb :find_contacts
 end
 
 get '/contacts/new' do
@@ -18,7 +38,74 @@ get '/contacts/new' do
 end
 
 post '/contacts' do
-	new_contact=Contact.new(params[:first_name],params[:last_name],params[:email],params[:notes])
-	@@rolodex.add_contact(new_contact)
+	new_contact=Contact.create(:first_name => params[:first_name],
+		:last_name => params[:last_name],
+		:email => params[:email],
+		:note => params[:note]
+		)
 	redirect to('/contacts')
 end
+
+get '/contacts/:id' do
+	@contact = Contact.get(params[:id].to_i)
+	if @contact
+		erb :show_contact
+	else
+		raise Sinatra::NotFound
+	end
+end
+
+get "/contacts/:id/edit" do
+	@contact = Contact.get(params[:id].to_i)
+	if @contact
+		erb :editcontact
+	else
+		raise Sinatra::NotFound
+	end
+end
+
+post '/contacts/:id' do
+	contact=Contact.get(params[:id].to_i)
+	contact.update(:first_name => params[:first_name])
+	contact.update(:last_name => params[:first_name])
+	contact.update(:email => params[:email])
+	contact.update(:note => params[:note])
+	redirect to("/contacts")
+end
+
+delete "/contacts/:id" do
+	contact = Contact.get(params[:id].to_i)
+	if contact
+		contact.destroy
+		redirect to("/contacts")
+	else
+		raise Sinatra::NotFound
+	end
+end
+
+post "/contacts/:id/edit" do
+	if @foundcontact.size > 0
+		erb :editcontact
+	else
+		raise Sinatra::NotFound
+	end
+end
+# get '/contacts/find' do
+# 	erb :find_contacts
+# end
+
+get '/search' do
+	if params[:first_name] || params[:last_name]
+		@contact = (Contact.all(:first_name => params[:first_name]) & Contact.all(:last_name => params[:last_name]))[0]
+		p @contact
+		puts "\n\n\n\n\n\n"
+		if @contact
+			erb :show_contact
+		else
+			raise Sinatra::NotFound
+		end
+	else
+		erb :find_contacts
+	end
+end
+
